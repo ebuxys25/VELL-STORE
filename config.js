70d578e7cfda129cab70adacb1267b52b65fa044
ebuxys25 <dedianair@gmail.com>
2026-01-26 17:22:24 +0700

    1

--- config.js
+++ config.js
@@ -6,17 +6,20 @@
 // Link Discord untuk tombol "Cek Rate" & "Beli"
 const DISCORD_INVITE_LINK = "https://discord.gg/5wKzxaydh6";
 
+// Link WhatsApp untuk tombol "Beli" (Optional)
+// Kosongkan "" kalau cuma mau pakai Discord
+const WHATSAPP_PHONE_NUMBER = "6283821170532"; 
+
 // 2. PENGATURAN RATE
 // Rate default Robux (Otomatis terisi di kalkulator)
 const DEFAULT_RATE = 85;
 
 // 3. PENGATURAN TAMPILAN
 // Ganti background sesuai keinginan. Bisa pakai:
-// - Nama file lokal: "background.jpg" (pastikan file ada di folder yg sama)
-// - Link gambar online: "https://link-gambar.com/gambar.jpg"
+// - Nama file lokal: "background.jpg"
+// - Link gambar online
 const BACKGROUND_IMAGE = "background.jpg"; 
 
 // ==========================================
 // AKHIR KONFIGURASI
-// JANGAN UBAH KODE DI BAWAH INI KECUALI PAHAM JS
 // ==========================================
