80048380d76a6eb28dca766c51e576cc6a4c9468
ebuxys25 <dedianair@gmail.com>
2026-01-26 16:17:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// File: config.js
+// Di sini tempat settingan utama RBX MARKET
+
+// Link Invite Discord (Beli & Cek Rate)
+const DISCORD_INVITE_LINK = "https://discord.gg/5wKzxaydh6";
+
+// Rate Default Robux (Akan muncul otomatis di kalkulator)
+const DEFAULT_RATE = 85;
